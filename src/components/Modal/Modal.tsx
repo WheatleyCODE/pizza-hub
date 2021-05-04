ca89baf594421dcff27592d9995a6a641b228dd5
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2021-05-04 16:24:02 +0300

    Fix modal scroll

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import BackDrop from '../UI/BackDrop/BackDrop';
 import './Modal.scss';
 
@@ -7,18 +7,25 @@ interface IModalProps {
   onCloseModal: () => void,
 }
 
-const Modal = ({ children, onCloseModal }: IModalProps) => (
-  <>
-    <div className="Modal">
-      <div className="Modal__main">
-        <button onClick={onCloseModal} type="button" className="closeButton">
-          <i className="fa fa-times" aria-hidden="true" />
-        </button>
-        { children }
+const Modal = ({ children, onCloseModal }: IModalProps) => {
+  useEffect(() => {
+    document.body.classList.add('modal-open');
+    return () => document.body.classList.remove('modal-open');
+  }, []);
+
+  return (
+    <>
+      <div className="Modal">
+        <div className="Modal__main">
+          <button onClick={onCloseModal} type="button" className="closeButton">
+            <i className="fa fa-times" aria-hidden="true" />
+          </button>
+          { children }
+        </div>
+        <BackDrop onCloseHandler={onCloseModal} />
       </div>
-      <BackDrop onCloseHandler={onCloseModal} />
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Modal;
