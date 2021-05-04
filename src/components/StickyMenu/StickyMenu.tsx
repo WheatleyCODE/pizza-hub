6b0035612b51bd3b5e189cc1f4f7be13f319eeb0
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2021-05-04 11:59:12 +0300

    Add new logo, fix logo animation

--- src/components/StickyMenu/StickyMenu.tsx
+++ src/components/StickyMenu/StickyMenu.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import stichyMenuItems from '../../utils/stichyMenuItems';
 import Button from '../UI/Button/Button';
 import logoImg from '../../img/pizza.png';
@@ -25,8 +26,16 @@ const StickyMenu = () => {
     <div className="StickyMenu">
       <div className="StickyMenu__container">
         <div className="flex">
-          { showLogo ? <div className="StickyMenu__logo"><img src={logoImg} alt="logo" /></div> : null }
-          <ul className={`menu ${showLogo ? 'translate' : ''}`}>
+          <CSSTransition
+            in={showLogo}
+            timeout={150}
+            classNames="modal"
+            mountOnEnter
+            unmountOnExit
+          >
+            <div className="StickyMenu__logo"><img src={logoImg} alt="logo" /></div>
+          </CSSTransition>
+          <ul className={`menu ${showLogo ? 'translate' : 'unTranslate'}`}>
             { stichyMenuItems.map((item) => (
               <li className="menu__list" key={item.title}>{item.title}</li>
             )) }
