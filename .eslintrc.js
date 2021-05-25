cc57e1cbe1cc20f9406b5f1da0b48d655bc9ddf4
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2021-05-25 11:48:15 +0300

    Add eslint extends, add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,44 @@
-module.exports =  {
-  parser:  '@typescript-eslint/parser', 
-  extends:  [
-    'airbnb-typescript',
+module.exports = {
+  "extends": [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
   ],
-  
- parserOptions:  {
-    project: './tsconfig.json',
+  "plugins": ["react", "@typescript-eslint", "jest", "prettier"],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module",
+    "project": "./tsconfig.json"
   },
-  rules: {
-    "react/require-default-props": [0],
-    "react/no-array-index-key": [0],
-    "react/jsx-props-no-spreading": [0],
-    "import/prefer-default-export": [0],
-  }
+  "rules": {
+    "import/prefer-default-export": 0,
+    "react/jsx-one-expression-per-line": 0,
+    "react/state-in-constructor": 0,
+    "react/jsx-props-no-spreading": 0,
+    "no-console": ["warn", { "allow": ["warn", "error"] }],
+    "operator-linebreak": ["error", "before", { "overrides": { "=": "ignore" } }],
+
+    "max-len": ["error", {
+        "code": 100,
+        "tabWidth": 2,
+        "ignoreStrings": true,
+        "ignoreTemplateLiterals": true
+      }
+    ]
+  },
+  "overrides": [
+    {
+      "files": ["**/*.tsx"],
+      "rules": {
+        "react/prop-types": "off"
+      }
+    }
+]
 }
